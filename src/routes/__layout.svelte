324eaf4c70f6581e8bfc14067c62245a43581943
entraptadoeztech <272025446+entraptadoeztech@users.noreply.github.com>
2026-09-17 18:38:00 -0500

    Update __layout.svelte

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,20 +1,3 @@
 <script lang="ts">
 	import '../styles/global.css';
 </script>
-
-<svelte:window
-	on:sveltekit:navigation-start={() => {
-		$navigationState = 'loading';
-	}}
-	on:sveltekit:navigation-end={() => {
-		$navigationState = 'loaded';
-	}}
-/>
-
-{#if $navigationState === 'loading'}
-	<div out:fade={{ delay: 500 }}>
-		<PageLoader />
-	</div>
-{/if}
-
-<slot />
