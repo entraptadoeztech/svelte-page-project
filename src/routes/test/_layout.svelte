d1e7aec0ebced38097fd4410d5bbd692bfc609cc
entraptadoeztech <272025446+entraptadoeztech@users.noreply.github.com>
2026-09-17 18:43:01 -0500

    Update _layout.svelte

--- src/routes/test/_layout.svelte
+++ src/routes/test/_layout.svelte
@@ -1,24 +1,5 @@
 <script lang="ts">
-	import { fade } from 'svelte/transition';
-
-	import navigationState from '../stores/navigationState';
-	import PageLoader from '../components/PageLoader.svelte';
-	import 'https://raw.githubusercontent.com/entraptadoeztech/sveltekit-page-progress-demo/refs/heads/main/src/styles/test.css';
+	import '../styles/test.css';
 </script>
-
-<svelte:window
-	on:sveltekit:navigation-start={() => {
-		$navigationState = 'loading';
-	}}
-	on:sveltekit:navigation-end={() => {
-		$navigationState = 'loaded';
-	}}
-/>
-
-{#if $navigationState === 'loading'}
-	<div out:fade={{ delay: 500 }}>
-		<PageLoader />
-	</div>
-{/if}
 
 <slot />
