c6dbfe0bdfbad37048ec40e452aec0d40657fc02
entraptadoeztech <272025446+entraptadoeztech@users.noreply.github.com>
2026-09-17 18:20:55 -0500

    Update _layout.svelte

--- src/routes/test/_layout.svelte
+++ src/routes/test/_layout.svelte
@@ -3,7 +3,7 @@
 
 	import navigationState from '../stores/navigationState';
 	import PageLoader from '../components/PageLoader.svelte';
-	import '../styles/global.css';
+	import '../styles/test.css';
 </script>
 
 <svelte:window
