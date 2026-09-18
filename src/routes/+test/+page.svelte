5b375f856ca436ae3926efec7af0a6d56a18058b
entraptadoeztech <272025446+entraptadoeztech@users.noreply.github.com>
2026-09-17 20:06:33 -0500

    Update +page.svelte

--- src/routes/+test/+page.svelte
+++ src/routes/+test/+page.svelte
@@ -1,7 +1,9 @@
 <h1>Testingggggg</h1>
 		<p>
 		this website loads faster than regular html websites(wow)
-		</p>
+        <img src="/logo.png" alt="Felix" />
+
+</p>
 		<div class="links">
 			<a href="/">Home</a>
 			<a href="/page-2">Page 2</a>
