a934b78e6f8fb89fb44f3d4178ca1f3cde60bec2
entraptadoeztech <272025446+entraptadoeztech@users.noreply.github.com>
2026-09-17 19:00:07 -0500

    Update +page.svelte

--- src/routes/+test/+page.svelte
+++ src/routes/+test/+page.svelte
@@ -1,4 +1,8 @@
-    <h1>Testingggggg</h1>
+<script lang="ts">
+	import '.../styles/test.css';
+</script>
+
+<h1>Testingggggg</h1>
 		<p>
 		this website loads faster than regular html websites(wow)
 		</p>
